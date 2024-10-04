c5c8da5164f4263d18e99fb6fb8d93368dc9218c
allyntreshansky <enquiries@metoffice.gov.uk>
2024-10-04 12:16:54 +0000

    ticket:330 for Benjamin Went: Investigate CCE opt options
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@4035 3896877f-8c05-4358-bc88-7947bb4308c7

--- applications/lfric_atm/build/fortran/crayftn.mk
+++ applications/lfric_atm/build/fortran/crayftn.mk
@@ -10,12 +10,6 @@ $(info Project specials for Cray compiler)
 
 export FFLAGS_UM_PHYSICS = -s real64
 
-# The lfric_atm app defines an extra set of debug flags for
-# fast-debug. For this compiler use the same as the full-debug
-# settings
-FFLAGS_FASTD_INIT         = $(FFLAGS_INIT) 
-FFLAGS_FASTD_RUNTIME      = $(FFLAGS_RUNTIME)
-
 #to try and ease compile time for CCE on EXZ
 ifeq ($(shell expr ${CRAYFTN_VERSION} \>= 015000000), 1)
     %bl_imp_alg_mod_psy.o %bl_imp_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
@@ -23,11 +17,45 @@ ifeq ($(shell expr ${CRAYFTN_VERSION} \>= 015000000), 1)
     %ukca_emiss_mode_mod.o %ukca_emiss_mode_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
     %ukca_emiss_mode_mod.o %ukca_emiss_mode_mod.mod: private FFLAGS_DEBUG = -G0
     %bl_exp_alg_mod_psy.o %bl_exp_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0 
-    %bl_exp_alg_mod_psy.o %bl_exp_alg_mod_psy.mod: privateFFLAGS_DEBUG = -G0
+    %bl_exp_alg_mod_psy.o %bl_exp_alg_mod_psy.mod: private FFLAGS_DEBUG = -G0
+    %aerosol_ukca_alg_mod_psy.o %aerosol_ukca_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %aerosol_ukca_alg_mod_psy.o %aerosol_ukca_alg_mod_psy.mod: private FFLAGS_DEBUG = -G0
     %init_aerosol_fields_alg_mod_psy.o init_aerosol_fields_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0 
     %init_aerosol_fields_alg_mod_psy.o init_aerosol_fields_alg_mod_psy.mod: private FFLAGS_DEBUG = -G0
     %jules_extra_kernel_mod.o %jules_extra_kernel_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0 
     %jules_extra_kernel_mod.o %jules_extra_kernel_mod.mod: private FFLAGS_DEBUG = -G0
- endif
-$(info $(FFLAGS_SAFE_OPTIMISATION))
-$(info $(FFLAGS_DEBUG))
+    %conv_gr_alg_mod_psy.o %conv_gr_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %conv_gr_alg_mod_psy.o %conv_gr_alg_mod_psy.mod: private FFLAGS_DEBUG = -G0
+    %gungho_model_mod.o %gungho_model_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %gungho_model_mod.o %gungho_model_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_collections_mod.o %driver_collections_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_collections_mod.o %driver_collections_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_comm_mod.o %driver_comm_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_comm_mod.o %driver_comm_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_config_mod.o %driver_config_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_config_mod.o %driver_config_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_coordinates_mod.o %driver_coordinates_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_coordinates_mod.o %driver_coordinates_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_counter_mod.o %driver_counter_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_counter_mod.o %driver_counter_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_fem_mod.o %driver_fem_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_fem_mod.o %driver_fem_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_io_mod.o %driver_io_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_io_mod.o %driver_io_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_log_mod.o %driver_log_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_log_mod.o %driver_log_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_mesh_mod.o %driver_mesh_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_mesh_mod.o %driver_mesh_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_model_data_mod.o %driver_model_data_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_model_data_mod.o %driver_model_data_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_modeldb_mod.o %driver_modeldb_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_modeldb_mod.o %driver_modeldb_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_time_mod.o %driver_time_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_time_mod.o %driver_time_mod.mod: private FFLAGS_DEBUG = -G0
+    %driver_timer_mod.o %driver_timer_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %driver_timer_mod.o %driver_timer_mod.mod: private FFLAGS_DEBUG = -G0
+    %io_context_collection_mod.o %io_context_collection_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %io_context_collection_mod.o %io_context_collection_mod.mod: private FFLAGS_DEBUG = -G0
+    %variable_fields_mod.o %variable_fields_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %variable_fields_mod.o %variable_fields_mod.mod: private FFLAGS_DEBUG = -G0
+endif
